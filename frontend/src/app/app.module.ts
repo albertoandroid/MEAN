77681e460e753120113eba94cc585ba13eee4f10
Alberto <info@androiddesdecero.com>
2019-11-23 17:38:43 +0100

    3

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { CreateTaskComponent } from './task/create-task/create-task.component';
+import { ListTaskComponent } from './task/list-task/list-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent,
+    CreateTaskComponent,
+    ListTaskComponent
   ],
   imports: [
     BrowserModule,
