b054c888684965515830260d177099ea1e6479b6
Alberto <info@androiddesdecero.com>
2019-11-23 17:49:05 +0100

    4

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ListTaskComponent } from './task/list-task/list-task.component';
+import { CreateTaskComponent } from './task/create-task/create-task.component';
+import { SignupComponent } from './signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'tasks',
+    component: ListTaskComponent
+  },
+  {
+    path: 'create',
+    component: CreateTaskComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
