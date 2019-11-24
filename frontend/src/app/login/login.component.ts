f901e86bed20a20d1f62e5ab48bde02e4b214fc8
Alberto <info@androiddesdecero.com>
2019-11-24 07:49:04 +0100

    11

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginUser = {}
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(){
+    
   }
 
 }
