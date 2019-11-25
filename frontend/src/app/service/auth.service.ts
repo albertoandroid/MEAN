f21855d4a4c87f02be99c92b5c8a8956a26ed6e2
Alberto <info@androiddesdecero.com>
2019-11-25 14:43:43 +0100

    41

--- frontend/src/app/service/auth.service.ts
+++ frontend/src/app/service/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Router } from '@angular/router'
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,8 @@ export class AuthService {
   private signUpURL = "http://localhost:3003/api/user"
   private loginURL = "http://localhost:3003/api/auth"
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private router: Router) { }
 
   signUpUser(user){
     return this.http.post<any>(this.signUpURL, user)
@@ -25,5 +27,10 @@ export class AuthService {
 
   getToken(){
     return localStorage.getItem('token')
+  }
+
+  logoutUser(){
+    localStorage.removeItem('token')
+    this.router.navigate(['/login'])
   }
 }
